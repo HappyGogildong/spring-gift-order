5b433b8ab66059da3db3d79aaf4f914a7e0edcb1
happyGogildong <rbgus630@gmail.com>
2025-07-29 02:37:04 +0900

    <feat>(Order Entity): Order 엔티티 생성

---
+++ src/main/java/gift/entity/Order.java
@@ -0,0 +1,35 @@
+package gift.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+
+    @Column(nullable = false)
+    @Min(1)
+    @Max(1000000000 - 1)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDateTime;
+}
