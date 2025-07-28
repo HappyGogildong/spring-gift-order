12291e979d7990e5a9c0f20d41f36949e2bbd9c2
happyGogildong <rbgus630@gmail.com>
2025-07-29 02:40:59 +0900

    <feat>(토큰 table): token을 저장해둘 테이블 생성

---
+++ src/main/java/gift/entity/MemberToken.java
@@ -0,0 +1,61 @@
+package gift.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member_token")
+public class MemberToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String accessToken;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private LocalDateTime accessTokenexpiresAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+    public String getAccessToken() {
+        return accessToken;
+    }
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+    public void setAccessTokenexpiresAt(LocalDateTime accessTokenexpiresAt) {
+        this.accessTokenexpiresAt = accessTokenexpiresAt;
+    }
+    public LocalDateTime getAccessTokenexpiresAt() {
+        return accessTokenexpiresAt;
+    }
+}
