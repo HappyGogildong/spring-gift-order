1e5d8febb1970f7b94ecb644f7b304e9991c53b4
happyGogildong <rbgus630@gmail.com>
2025-07-29 16:56:04 +0900

    <feat>(카카오 토큰 dto): 카카오API로 받은 토큰 응답 양식 dto

--- src/main/java/gift/dto/response/KakaoAuthTokenResponse.java
+++ src/main/java/gift/dto/response/KakaoAuthTokenResponse.java
@@ -1,11 +1,18 @@
 package gift.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoAuthTokenResponse(
-    String token_type,
-    String access_token,
-    int expires_in,
-    String refresh_token,
-    int refresh_token_expires_in,
+
+    String tokenType,
+    String accessToken,
+    int expiresIn,
+    String refreshToken,
+    int refreshTokenExpiresIn,
     String scope
 ) {
 
