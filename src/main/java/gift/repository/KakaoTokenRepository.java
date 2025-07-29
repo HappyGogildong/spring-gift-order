024926aecd65289f953517e90ec1fb4fc745873a
happyGogildong <rbgus630@gmail.com>
2025-07-29 16:58:53 +0900

    <feat>(kakao token repository): 카카오 token 엔티티의 JPA repository

---
+++ src/main/java/gift/repository/KakaoTokenRepository.java
@@ -0,0 +1,11 @@
+package gift.repository;
+
+import gift.entity.MemberKakaoToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KakaoTokenRepository extends JpaRepository<MemberKakaoToken, Integer> {
+
+    String findAccessTokenByEmail(String email);
+}
